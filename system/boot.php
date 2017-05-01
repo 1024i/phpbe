2a02095f3cade11241050aa91fa5f5f296068ea4
USER-20161231FB\Administrator <8981024@qq.com>
2017-05-01 13:31:37 +0800

    增加 tool

--- system/boot.php
+++ system/boot.php
@@ -2,6 +2,7 @@
 
 use \system\be;
 use \system\request;
+use \system\response;
 
 require PATH_ROOT . DS . 'system' . DS . 'loader.php';
 spl_autoload_register(array('\\system\\loader', 'autoload'));
@@ -10,176 +11,170 @@ require PATH_ROOT . DS . 'system' . DS . 'tool.php';
 
 // 检查网站配置， 是否暂停服务
 $config_system = be::get_config('system');
-if ($config_system->offline === '1') be_exit($config_system->offline_message);
+if ($config_system->offline === '1') response::end($config_system->offline_message);
 
 if ($config_system->error_log !== '0') {
     set_error_handler('\system\error_log::error');
     set_exception_handler('\system\error_log::exception');
 }
 
-// 启动 session
-\system\session::start();
+try {
+    // 启动 session
+    \system\session::start();
 
-$my = be::get_user();
-if (!isset($my->id) || $my->id == 0) {
-    $model = be::get_model('user');
-    $model->remember_me();
-}
-
-//print_r($_SERVER);
-
-$uri = $_SERVER['REQUEST_URI'];    // 返回值为: /{controller}/{task}......
-$script_name = $_SERVER['SCRIPT_NAME'];
-if ($script_name != '/index.php') $uri = substr($uri, strrpos($script_name, '/index.php'));
-
-
-if ($config_system->sef !== '0') {
-    if ($_SERVER['QUERY_STRING'] != '') $uri = substr($uri, 0, strrpos($uri, '?'));
-
-    $len_sef_suffix = strlen($config_system->sef_suffix);
-    if (substr($uri, -$len_sef_suffix, $len_sef_suffix) == $config_system->sef_suffix) {
-        $uri = substr($uri, 0, strrpos($uri, $config_system->sef_suffix));
+    $my = be::get_user();
+    if (!isset($my->id) || $my->id == 0) {
+        $model = be::get_model('user');
+        $model->remember_me();
     }
 
-    if (substr($uri, -1, 1) == '/') $uri = substr($uri, 0, -1);
+    //print_r($_SERVER);
 
-    $uris = explode('/', $uri);
-    $len = count($uris);
-    if ($len >= 2) {
-        $controller = $uris[1];
-        $_GET['controller'] = $_REQUEST['controller'] = $controller;
+    $uri = $_SERVER['REQUEST_URI'];    // 返回值为: /{controller}/{task}......
+    $script_name = $_SERVER['SCRIPT_NAME'];
+    if ($script_name != '/index.php') $uri = substr($uri, strrpos($script_name, '/index.php'));
 
-        if ($len > 2) {
-            $router = be::get_router($controller);
-            $router->decode_url($uris);
+    if ($config_system->sef !== '0') {
+        if ($_SERVER['QUERY_STRING'] != '') $uri = substr($uri, 0, strrpos($uri, '?'));
+
+        $len_sef_suffix = strlen($config_system->sef_suffix);
+        if (substr($uri, -$len_sef_suffix, $len_sef_suffix) == $config_system->sef_suffix) {
+            $uri = substr($uri, 0, strrpos($uri, $config_system->sef_suffix));
+        }
+
+        if (substr($uri, -1, 1) == '/') $uri = substr($uri, 0, -1);
+
+        $uris = explode('/', $uri);
+        $len = count($uris);
+        if ($len >= 2) {
+            $controller = $uris[1];
+            $_GET['controller'] = $_REQUEST['controller'] = $controller;
+
+            if ($len > 2) {
+                $router = be::get_router($controller);
+                $router->decode_url($uris);
+            }
         }
     }
-}
 
-$controller = request::request('controller', '');
-$task = request::request('task', '');
+    $controller = request::request('controller', '');
+    $task = request::request('task', '');
 
-// 默认首页时
-if ($controller == '') {
-    $home_params = $config_system->home_params;
-    foreach ($home_params as $key => $val) {
-        $_GET[$key] = $_REQUEST[$key] = $val;
-        if ($key == 'controller') $controller = $val;
-        if ($key == 'task') $task = $val;
+    // 默认首页时
+    if ($controller == '') {
+        $home_params = $config_system->home_params;
+        foreach ($home_params as $key => $val) {
+            $_GET[$key] = $_REQUEST[$key] = $val;
+            if ($key == 'controller') $controller = $val;
+            if ($key == 'task') $task = $val;
+        }
     }
-}
 
-$instance = be::get_controller($controller);
-if ($instance === null) {
-    if (request::is_ajax()) {
-        $response = new \stdClass();
-        $response->error = -404;
-        $response->message = '页面不存在！';
-        $response->redirect_url = URL_ROOT . '/404.html';
-        echo json_encode($response);
-    } else {
-        header('location: ' . URL_ROOT . '/404.html');
+    $instance = be::get_controller($controller);
+    if ($instance === null) {
+        if (request::is_ajax()) {
+            response::set('error', -404);
+            response::set('message', '页面不存在！');
+            response::set('redirect_url', URL_ROOT . '/404.html');
+            response::ajax();
+        } else {
+            response::redirect(URL_ROOT . '/404.html');
+        }
     }
-    exit;
-}
 
-if ($task == '') $task = 'index';
-if (method_exists($instance, $task)) {
-    /*
-    $permission_key = $controller.'.'.$task;
+    if ($task == '') $task = 'index';
+    if (method_exists($instance, $task)) {
+        /*
+        $permission_key = $controller.'.'.$task;
 
-    $app_name = $controller;
-    $pos = strpos($controller, '_');
-    if ($pos! == false) $app_name = substr($controller, 0, $pos);
+        $app_name = $controller;
+        $pos = strpos($controller, '_');
+        if ($pos! == false) $app_name = substr($controller, 0, $pos);
 
-    $app = be::get_app($app_name);
+        $app = be::get_app($app_name);
 
-    // 检查用户权限
-    $role = be::get_role($my->role_id);
-    $role_permissions = $role->get_permissions();
+        // 检查用户权限
+        $role = be::get_role($my->role_id);
+        $role_permissions = $role->get_permissions();
 
-    if (isset($config_user_group->$permissions_field_name)) {
-        $permissions = $config_user_group->$permissions_field_name;
-        if (is_array($permissions)) {
-            if ($app) {
-                $matched_app_permission = null;
-                $app_permissions = $app->get_permissions();
-                foreach ($app_permissions as $app_permission) {
-                    if (in_array($permission_key, $app_permission[1])) {
-                        $matched_app_permission = $app_permission;
-                        break;
+        if (isset($config_user_group->$permissions_field_name)) {
+            $permissions = $config_user_group->$permissions_field_name;
+            if (is_array($permissions)) {
+                if ($app) {
+                    $matched_app_permission = null;
+                    $app_permissions = $app->get_permissions();
+                    foreach ($app_permissions as $app_permission) {
+                        if (in_array($permission_key, $app_permission[1])) {
+                            $matched_app_permission = $app_permission;
+                            break;
+                        }
                     }
-                }
 
-                if ($matched_app_permission === null) {
-                    $permission_text = '您没有权限（该功能未加入权限管理系统）！';
-                } else {
-
-
-                }
-
-
-                if (isset($app_permission_maps[$permission_key])) {
-                    $app_permission_key = $app_permission_maps[$permission_key];
-                    if ($app_permission_key == '-' || in_array($app_permission_key, $permissions)) {
-                        $permission = true;
+                    if ($matched_app_permission === null) {
+                        $permission_text = '您没有权限（该功能未加入权限管理系统）！';
                     } else {
-                        $app_permissions = $app->get_permissions();
-                        $permission_text = '您没有权限：' . $app_permissions[$app_permission_key];
+
+
                     }
+
+
+                    if (isset($app_permission_maps[$permission_key])) {
+                        $app_permission_key = $app_permission_maps[$permission_key];
+                        if ($app_permission_key == '-' || in_array($app_permission_key, $permissions)) {
+                            $permission = true;
+                        } else {
+                            $app_permissions = $app->get_permissions();
+                            $permission_text = '您没有权限：' . $app_permissions[$app_permission_key];
+                        }
+                    }
+                } else {
+                    $permission_text = '您没有权限（该功能未加入权限管理系统）！';
                 }
             } else {
-                $permission_text = '您没有权限（该功能未加入权限管理系统）！';
+                // 1: 所有权限 0或其它值:没有任何权限
+                if ($permissions == '1') $permission = true;
             }
-        } else {
-            // 1: 所有权限 0或其它值:没有任何权限
-            if ($permissions == '1') $permission = true;
         }
-    }
 
 
-    if (!$permission && $app) {
-        $app_permission_maps = $app->get_permission_maps();
-        if (isset($app_permission_maps[$permission_key]) && $app_permission_maps[$permission_key] == '-') $permission = true;
-    }
-
-
-    if (!$permission) {
-        if ($permission_text == '') $permission_text = '您没有权限！';
-
-        if (request::is_ajax()) {
-            $response = new \stdClass();
-            $response->error = -1024;
-            $response->message = $permission_text;
-            echo json_encode($response);
-            exit();
-        } else {
-            be_exit($permission_text);
+        if (!$permission && $app) {
+            $app_permission_maps = $app->get_permission_maps();
+            if (isset($app_permission_maps[$permission_key]) && $app_permission_maps[$permission_key] == '-') $permission = true;
         }
-    }
-    */
 
-    try {
+
+        if (!$permission) {
+            if ($permission_text == '') $permission_text = '您没有权限！';
+
+            if (request::is_ajax()) {
+                $response = new \stdClass();
+                $response->error = -1024;
+                $response->message = $permission_text;
+                echo json_encode($response);
+                exit();
+            } else {
+                be_exit($permission_text);
+            }
+        }
+        */
+
         $instance->$task();
-    } catch (Exception $e) {
+
+    } else {
         if (request::is_ajax()) {
-            $response = new \stdClass();
-            $response->error = -500;
-            $response->message = $e->getMessage();
-            echo json_encode($response);
+            response::set('error', -404);
+            response::set('message', '未定义的任务：' . $task);
+            response::ajax();
         } else {
-            be_exit($e->getMessage());
+            response::end('未定义的任务：' . $task);
         }
     }
-
-} else {
+} catch (Throwable $t) {
     if (request::is_ajax()) {
-        $response = new \stdClass();
-        $response->error = -404;
-        $response->message = '未定义的任务：' . $task;
-        echo json_encode($response);
+        response::set('error', -500);
+        response::set('message', $t->getMessage());
+        response::ajax();
     } else {
-        be_exit('未定义的任务：' . $task);
+        response::end($t->getMessage());
     }
 }
-
---
+++ tool/system.php
@@ -0,0 +1,179 @@
+<?php
+namespace tool;
+
+
+class system
+{
+    /**
+     * 处理网址
+     *
+     * @param string $url 要处理的网址，启用 SEF 时生成伪静态页， 为空时返回网站网址
+     * @return string
+     */
+    public static function url($url)
+    {
+        $config_system = \system\be::get_config('system');
+        if ($config_system->sef !== '0') {
+            $urls = explode('&', $url);
+
+            if (count($urls) == 0) return URL_ROOT;
+
+            $controller = null;
+            $task = null;
+            $params = array();
+
+            foreach ($urls as $x) {
+                $pos = strpos($x, '=');
+
+                if ($pos !== false) {
+                    $key = substr($x, 0, $pos);
+                    $val = substr($x, $pos+1);
+
+                    if ($key == 'controller') {
+                        $controller = $val;
+                    } elseif ($key == 'task') {
+                        $task = $val;
+                    } else {
+                        $params[$key] = $val;
+                    }
+                }
+            }
+
+            if ($controller == null) return URL_ROOT;
+            if ($task == null) return URL_ROOT . '/' . $controller . $config_system->sef_suffix;
+
+            $router = \system\be::get_router($controller);
+            return $router->encode_url($controller, $task, $params);
+        }
+
+        return URL_ROOT . '/?' . $url;
+    }
+
+
+
+    /**
+     *
+     * 限制字符串宽度
+     * 名词说明
+     * 字符: 一个字符占用一个字节， strlen 长度为 1
+     * 文字：(可以看成由多个字符组成) 占用一个或多个字节  strlen 长度可能为 1,2,3,4,5,6
+     *
+     * @param string $string 要限制的字符串
+     * @param int $length 限制的宽度
+     * @param string $etc 结层符号
+     * @return string
+     */
+    public static function limit($string, $length = 50, $etc = ' .  .  . ')
+    {
+        $string = strip_tags($string);
+        $length *= 2; //按中文时宽度应加倍
+
+
+        if (strlen($string) <= $length) return $string;
+
+        $length -= strlen($etc); // 去除结尾符长度
+        if ($length <= 0) return '';
+
+        $str_len = strlen($string);
+
+        $pos = 0; // 当前处理到的字符位置
+        $last_len = 0; // 最后一次处理的字符所代表的文字的宽度
+        $len = 0; // 文字宽度累加值
+
+
+        while ($pos < $str_len) // 系统采用了utf-8编码， 逐字符判断
+        {
+            $char = ord($string[$pos]);
+            if ($char == 9 || $char == 10 || (32 <= $char && $char <= 126)) {
+                $last_len = 1;
+                $pos++;
+                $len++;
+            }
+            elseif (192 <= $char && $char <= 223) {
+                $last_len = 2;
+                $pos += 2;
+                $len += 2;
+            }
+            elseif (224 <= $char && $char <= 239) {
+                $last_len = 3;
+                $pos += 3;
+                $len += 2;
+            }
+            elseif (240 <= $char && $char <= 247) {
+                $last_len = 4;
+                $pos += 4;
+                $len += 2;
+            }
+            elseif (248 <= $char && $char <= 251) {
+                $last_len = 5;
+                $pos += 5;
+                $len += 2;
+            }
+            elseif ($char == 252 || $char == 253) {
+                $last_len = 6;
+                $pos += 6;
+                $len += 2;
+            } else {
+                $pos++;
+            }
+
+            if ($len >= $length) break;
+        }
+
+        // 超过指定宽度， 减去最后一次处理的字符所代表的文字宽度
+        if ($len >= $length) {
+            $pos -= $last_len;
+            $string = substr($string, 0, $pos);
+            $string .= $etc;
+        }
+
+        return $string;
+    }
+
+
+    /**
+     * 格式化时间
+     *
+     * @param int $time unix 时间戳
+     * @param int $max_days 多少天前或后以默认时间格式输出
+     * @param string $default_format 默认时间格式
+     * @return string
+     */
+    public static function format_time($time, $max_days = 30, $default_format = 'Y-m-d')
+    {
+        $t = time();
+
+        $seconds = $t-$time;
+
+        // 如果是{$max_days}天前，直接输出日期
+        $max_seconds = $max_days*86400;
+        if ($seconds > $max_seconds || $seconds <- $max_seconds) return date($default_format, $time);
+
+        if ($seconds > 86400) {
+            $days = intval($seconds / 86400);
+            if ($days == 1) {
+                if (date('a', $time) == 'am') return '昨天上午';
+                else return '昨天下午';
+            } elseif ($days == 2) {
+                return '前天';
+            }
+            return $days . '天前';
+        }
+        elseif ($seconds > 3600) return intval($seconds / 3600) . '小时前';
+        elseif ($seconds > 60) return intval($seconds / 60) . '分钟前';
+        elseif ($seconds >= 0) return '刚才';
+        elseif ($seconds > - 0) return '马上';
+        elseif ($seconds > -3600) return intval(-$seconds / 60) . '分钟后';
+        elseif ($seconds > -86400) return intval(-$seconds / 3600) . '小时后';
+        else {
+            $days = intval(-$seconds / 86400);
+            if ($days == 1) {
+                if (date('a', $time) == 'am') return '明天上午';
+                else return '明天下午';
+            } elseif ($days == 2) {
+                return '后天';
+            }
+            return $days . '天后';
+        }
+    }
+}
