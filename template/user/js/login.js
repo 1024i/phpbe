7186ff87fcd9acad0768daddf51ff3e256b93dd0
liuguoqiang <8981024@qq.com>
2017-05-22 17:03:08 +0800

    ajax 代码谳整

--- template/user/js/login.js
+++ template/user/js/login.js
@@ -1,59 +1,59 @@
+$(function () {
 
-$(function(){
+    $("#form-login").validate({
+        rules: {
+            username: {
+                required: true
+            },
+            password: {
+                required: true
+            },
+            captcha: {
+                required: true
+            }
+        },
+        messages: {
+            username: {
+                required: "请输入用户名！"
+            },
+            password: {
+                required: "请输入密码！"
+            },
+            captcha: {
+                required: "请输入验证码！"
+            }
+        },
 
-	$("#form-login").validate({
-		rules: {
-			username: {
-				required: true
-			},
-			password: {
-				required: true
-			},
-			captcha: {
-				required: true
-			}
-		},
-		messages: {
-			username: {
-				required: "请输入用户名！"
-			},
-			password: {
-				required: "请输入密码！"
-			},
-			captcha: {
-				required: "请输入验证码！"
-			}
-		},
-		
-		
-		submitHandler: function(form){
-			
-			var $submit = $(".btn-submit", $(form));
-			var sValue = $submit.val();
-			
-			$submit.prop("disabled", true).val("处理中，请稍候...");
 
-			$.ajax({
-				type: "POST",
-				url: URL_ROOT + "/?controller=user&task=ajax_login_check",
-				data: $(form).serialize(),
-				dataType: "json",
-				success: function(json){
-					
-					$submit.prop("disabled", false).val(sValue);
-					
-					if(json.error=="0")
-					{
-						window.location.href = json.redirect_url;
-					}
-					else
-					{
-						alert( json.message );
-					}
-				}
-			});	
+        submitHandler: function (form) {
 
-		}
-	});
+            var $submit = $(".btn-submit", $(form));
+            var sValue = $submit.val();
+
+            $submit.prop("disabled", true).val("处理中，请稍候...");
+
+            $.ajax({
+                type: "POST",
+                url: URL_ROOT + "/?controller=user&task=login_check",
+                data: $(form).serialize(),
+                dataType: "json",
+                success: function (json) {
+
+                    $submit.prop("disabled", false).val(sValue);
+
+                    if (json.error == "0") {
+                        window.location.href = json.redirect_url;
+                    }
+                    else {
+                        alert(json.message);
+                    }
+                },
+                error: function () {
+                    alert("服务器错误！")
+                }
+            });
+
+        }
+    });
 });
 
--- template/user_profile/js/edit_avatar.js
+++ template/user_profile/js/edit_avatar.js
@@ -1,11 +1,13 @@
-function deleteAvatar()
-{
-	$.ajax({
-		type: "GET",
-		url: URL_ROOT+"/?controller=user_profile&task=ajax_delete_avatar",
-		dataType: "json",
-		success: function(json){
-			alert( json.message );
-		}
-	});	
+function deleteAvatar() {
+    $.ajax({
+        type: "GET",
+        url: URL_ROOT + "/?controller=user_profile&task=ajax_delete_avatar",
+        dataType: "json",
+        success: function (json) {
+            alert(json.message);
+        },
+        error: function () {
+            alert("服务器错误！")
+        }
+    });
 }
