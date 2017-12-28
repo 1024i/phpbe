66a72d06565aaea08e6eff845742e2a6c82b2506
刘一二 <liuguoqiang@haitun.hk>
2017-12-28 19:53:40 +0800

    拆分 package

--- system/db/exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace system\db;
-
-/**
- * 错误异常
- */
-class Exception extends \Exception
-{
-}
--- system/exception/exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace system\Exception;
-
-/**
- * 自定义异常，用于处理业务逻辑时抛出
- */
-class CustomException extends \Exception
-{
-}
