d82acf9ab5f7bb19533f9cab73628939e0f14222
刘一二 <8981024@qq.com>
2018-01-30 12:28:02 +0800

    system 移回项目内

--- index.test.php
+++ index.test.php
@@ -2,4 +2,4 @@
 define('ENVIRONMENT', 'test'); // 测试环境
 require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'define.php';
 require PATH_ROOT .'/vendor/autoload.php';
-require PATH_ROOT . '/vendor/phpbe/system/src/Boot.php';
+require PATH_ROOT . '/system/Boot.php';
