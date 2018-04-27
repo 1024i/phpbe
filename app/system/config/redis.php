10bdba4b38234d802c98997be999fb87528dd514
刘一二 <8981024@qq.com>
2018-04-27 15:39:40 +0800

    Redis 多数据库配置

--- app/system/config/redis.php
+++ app/system/config/redis.php
@@ -3,10 +3,29 @@ namespace App\System\Config;
 
 class Redis
 {
-    public $host = '127.0.0.1'; // 主机名
-    public $port = 6379; // 端口号
-    public $timeout = 0;  // 超时时间
-    public $persistent = false;  // 是否使用长连接
-    public $password = '';  // 密码，不需要时留空
-    public $db = 0;  // 默认选中的数据库接
+
+    public $master = [
+        'host' => '172.24.0.110', // 主机名
+        'port' => 6379, // 端口号
+        'timeout' => 60, // 超时时间
+        'persistent' => false, // 是否使用长连接
+        'password' => '', // 密码，不需要时留空
+        'db' => 0 // 默认选中的数据库接
+    ];
+
+
+    public function __construct()
+    {
+        if (ENV == 'prod') {
+            $this->master = [
+                'host' => '127.0.0.1', // 主机名
+                'port' => 6379, // 端口号
+                'timeout' => 60, // 超时时间
+                'persistent' => false, // 是否使用长连接
+                'password' => '', // 密码，不需要时留空
+                'db' => 0 // 默认选中的数据库接
+            ];
+        }
+    }
+
 }
